98352c00f414915e376bc53f7ded4b8fda7d74e6
unknown <tomuthu.engineer@gmail.com>
2024-01-03 11:56:17 +0530

    Mastering List

---
+++ Python/LIST/extend_list.py
@@ -0,0 +1,9 @@
+# List extend() Method --> it adds the specified list elements to end of the list
+
+numbers = [1,2,3,4,5]
+
+def extend():
+    extend_list =[6,7,8,9]
+    numbers.extend(extend_list)
+    return numbers
+print(extend())

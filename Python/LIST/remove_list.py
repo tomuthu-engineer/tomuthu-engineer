9d00b853b53f2a1f0a9e04e003f86bbda2d8b92c
unknown <tomuthu.engineer@gmail.com>
2024-01-04 06:07:32 +0530

    Mastering List

---
+++ Python/LIST/remove_list.py
@@ -0,0 +1,8 @@
+# list remove() method --> it returns the first occurrence of the specified value
+
+colors = ['blue','blue','red','yellow']
+
+def remove_list():
+   colors.remove('blue')
+   return colors
+print(remove_list())

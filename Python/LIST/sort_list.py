b8333be72f93579bd3925122c12b8dae5e38c6b1
unknown <tomuthu.engineer@gmail.com>
2024-01-04 06:26:41 +0530

    Mastering List

---
+++ Python/LIST/sort_list.py
@@ -0,0 +1,8 @@
+# List sort() method --> it sorts the list elements
+
+numbers = [1,3,8,3,6,5,5,9,9,4,6,1,7,6,1,8,7,6,3,7,9,4,5,2] 
+
+def sort_list():
+    numbers.sort()
+    return numbers
+print(sort_list())
